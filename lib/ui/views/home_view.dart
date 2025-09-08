a1c37f7d150a95fbd637640e758b73a3c891bfd9
Camilo <camiloesan@gmail.com>
2025-09-08 13:01:18 -0600

    app bar with menu

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 part '../controllers/home_controller.dart';
 
+const Color primaryColor = Color(0xFF003466);
+
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
@@ -15,33 +17,78 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+        backgroundColor: Colors.transparent,
+        actionsPadding: EdgeInsets.only(right: 16.0),
+      ),
+      drawer: NavigationDrawer(
+        children: [
+          DrawerHeader(child: Image.asset('assets/images/head.png')),
+          ListTile(
+            leading: const Icon(Icons.home, color: primaryColor),
+            title: const Text('Inicio'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.book, color: primaryColor),
+            title: const Text('Biblioteca'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.history, color: primaryColor),
+            title: const Text('Historial de búsqueda'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.language, color: primaryColor),
+            title: const Text('Idioma'),
+            onTap: () {},
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.map, color: primaryColor),
+            title: const Text('Directorio de bibliotecas'),
+            trailing: Transform.scale(scale: 0.8, child: const Icon(Icons.open_in_new)),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.computer, color: primaryColor),
+            title: const Text('Recursos electrónicos'),
+            trailing: Transform.scale(scale: 0.8, child: const Icon(Icons.open_in_new)),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.help, color: primaryColor),
+            title: const Text('Preguntas frecuentes'),
+            trailing: Transform.scale(scale: 0.8, child: const Icon(Icons.open_in_new)),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.privacy_tip, color: primaryColor),
+            title: const Text('Aviso de privacidad'),
+            trailing: Transform.scale(scale: 0.8, child: const Icon(Icons.open_in_new)),
+            onTap: () {},
+          ),
+        ],
+      ),
+      drawerEnableOpenDragGesture: true,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FilledButton.tonal(
-                    onPressed: () {},
-                    style: FilledButton.styleFrom(
-                      backgroundColor: Colors.grey[200],
-                      // shape: const CircleBorder(),
-                      padding: const EdgeInsets.all(12),
-                      minimumSize: const Size(46, 46),
-                    ),
-                    child: Icon(Icons.menu, color: Colors.grey[600]),
-                  ),
-                  Expanded(
-                    child: SizedBox(
-                      height: 46,
-                      child: Image.asset('assets/images/head.png'),
-                    ),
-                  ),
-                ],
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Expanded(
+                child: SizedBox(
+                  height: 46,
+                  child: Image.asset('assets/images/head.png'),
+                ),
               ),
             ),
             DropdownMenu(
@@ -49,13 +96,13 @@ class _HomeViewState extends State<HomeView> {
               dropdownMenuEntries: ColorLabel.entries,
               width: double.infinity,
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16),
             DropdownMenu(
               label: const Text('Biblioteca'),
               dropdownMenuEntries: ColorLabel.entries,
               width: double.infinity,
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16),
             const TextField(
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.search),
@@ -66,7 +113,7 @@ class _HomeViewState extends State<HomeView> {
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16),
             Align(
               alignment: Alignment.centerLeft,
               child: Text(
@@ -74,7 +121,7 @@ class _HomeViewState extends State<HomeView> {
                 style: Theme.of(context).textTheme.headlineSmall,
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16),
             Align(
               alignment: Alignment.centerLeft,
               child: Text(
