d9c3ce59fb69cf0a3876a359732cacf57b830780
Camilo <camiloesan@gmail.com>
2025-09-09 14:49:16 -0600

    busqueda por titulo

--- lib/ui/controllers/search_controller.dart
+++ lib/ui/controllers/search_controller.dart
@@ -5,6 +5,7 @@ abstract class SearchController extends State<SearchView> {
   late TextEditingController _controllerTipoBusqueda;
   late TextEditingController _controllerBusqueda;
   late (String, String, String) searchQuery;
+  late List<String> _titulos = [];
 
   @override
   void initState() {
@@ -12,6 +13,7 @@ abstract class SearchController extends State<SearchView> {
     _controllerTipoBusqueda = TextEditingController();
     _controllerBiblioteca = TextEditingController();
     _controllerBusqueda = TextEditingController();
+    // can get the arguments before so its faster
   }
 
   @override
@@ -33,9 +35,59 @@ abstract class SearchController extends State<SearchView> {
     } else {
       searchQuery = ('', '', '');
     }
+    fetchXml();
     _controllerTipoBusqueda.text = searchQuery.$1;
     _controllerBiblioteca.text = searchQuery.$2;
     _controllerBusqueda.text = searchQuery.$3;
+  }
+
+  Future<void> fetchXml() async {
+    final url = Uri.parse('placeholder');
+    final response = await http.get(url);
+
+    print(url);
+
+    if (response.statusCode == 200) {
+    final document = xml.XmlDocument.parse(response.body);
+
+    const marcNamespace = "http://www.loc.gov/MARC21/slim";
+
+    final records = document.findAllElements(
+      "recordData",
+      namespace: "http://www.loc.gov/zing/srw/",
+    );
+
+    for (var recordData in records) {
+      final record = recordData.findElements("record", namespace: marcNamespace).firstOrNull;
+      
+      if (record == null) continue; // Skip if record not found
+
+      final datafield245 = record
+          .findElements("datafield", namespace: marcNamespace)
+          .firstWhereOrNull((df) => df.getAttribute("tag") == "245");
+
+      if (datafield245 == null) continue; // Skip if datafield245 not found
+
+      final subfieldA = datafield245
+          .findElements("subfield", namespace: marcNamespace)
+          .firstWhereOrNull((sf) => sf.getAttribute("code") == "a");
+
+      final subfieldB = datafield245
+          .findElements("subfield", namespace: marcNamespace)
+          .firstWhereOrNull((sf) => sf.getAttribute("code") == "b");
+
+      final subfieldC = datafield245
+          .findElements("subfield", namespace: marcNamespace)
+          .firstWhereOrNull((sf) => sf.getAttribute("code") == "c");
+
+      final title = "${subfieldA?.innerText ?? ''} ${subfieldB?.innerText ?? ''} ${subfieldC?.innerText ?? ''}";
+      _titulos.add(utf8.decode(title.trim().codeUnits));
+
+    }
+      setState(() {}); // Update UI after adding each title
+  } else {
+    throw Exception("Failed to load XML");
+  }
   }
 
   void onSubmitAction(String cadenaDeBusqueda) {
--- lib/ui/views/search_view.dart
+++ lib/ui/views/search_view.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart' as xml;
+import 'package:collection/collection.dart';
 
 part '../controllers/search_controller.dart';
 
@@ -19,48 +24,74 @@ class _SearchViewState extends SearchController {
 
     return Scaffold(
       appBar: AppBar(title: Text('Búsqueda')),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0),
-        child: Column(
-          children: [
-            DropdownMenu(
-              controller: _controllerTipoBusqueda,
-              label: const Text('Buscar por'),
-              leadingIcon: const Icon(Icons.filter_list, color: primaryColor),
-              dropdownMenuEntries: const [
-                DropdownMenuEntry(value: 'title', label: 'Título'),
-                DropdownMenuEntry(value: 'author', label: 'Autor'),
-              ],
-              width: double.infinity,
-            ),
-            const SizedBox(height: 8),
-            DropdownMenu(
-              controller: _controllerBiblioteca,
-              label: const Text('Biblioteca'),
-              leadingIcon: const Icon(Icons.location_city, color: primaryColor),
-              dropdownMenuEntries: const [
-                DropdownMenuEntry(
-                  value: 'homebranch',
-                  label: 'Todas las bibliotecas',
-                ),
-                DropdownMenuEntry(value: 'USBI-X', label: 'USBI Xalapa'),
-              ],
-              width: double.infinity,
-            ),
-            const SizedBox(height: 8),
-            TextField(
-              controller: _controllerBusqueda,
-              onSubmitted: (value) => onSubmitAction(value),
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search, color: primaryColor),
-                suffixIcon: Icon(Icons.clear),
-                labelText: 'Buscar',
-                border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              DropdownMenu(
+                controller: _controllerTipoBusqueda,
+                label: const Text('Buscar por'),
+                leadingIcon: const Icon(Icons.filter_list, color: primaryColor),
+                dropdownMenuEntries: const [
+                  DropdownMenuEntry(value: 'title', label: 'Título'),
+                  DropdownMenuEntry(value: 'author', label: 'Autor'),
+                ],
+                width: double.infinity,
               ),
-            ),
-            const SizedBox(height: 8),
-            Divider(color: Colors.grey),
-          ],
+              const SizedBox(height: 8),
+              DropdownMenu(
+                controller: _controllerBiblioteca,
+                label: const Text('Biblioteca'),
+                leadingIcon: const Icon(
+                  Icons.location_city,
+                  color: primaryColor,
+                ),
+                dropdownMenuEntries: const [
+                  DropdownMenuEntry(
+                    value: 'homebranch',
+                    label: 'Todas las bibliotecas',
+                  ),
+                  DropdownMenuEntry(value: 'USBI-X', label: 'USBI Xalapa'),
+                ],
+                width: double.infinity,
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: _controllerBusqueda,
+                onSubmitted: (value) => onSubmitAction(value),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search, color: primaryColor),
+                  suffixIcon: Icon(Icons.clear),
+                  labelText: 'Buscar',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Divider(color: Colors.grey),
+              const SizedBox(height: 8),
+              ..._titulos.map(
+                (title) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        onTap: () {},
+                        title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+                        subtitle: Text(
+                          'Por: Autor Desconocido \nDisponibilidad: 1 biblioteca \nOtro título: Título relacionado',
+                          style: TextStyle(fontWeight: FontWeight.normal),
+                        ),
+                        contentPadding: EdgeInsets.all(0),
+                        minVerticalPadding: 0,
+                      ),
+                      Divider(color: Colors.grey),
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
