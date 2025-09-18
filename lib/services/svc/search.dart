d8bbaec01087481e983ecb1063ca0df9d6fbdf11
Luis Casas <zs21013875@estudiantes.uv.mx>
2025-09-18 14:21:06 -0600

    make base url constant

--- lib/services/svc/search.dart
+++ lib/services/svc/search.dart
@@ -7,11 +7,12 @@ import 'package:catbiblio_app/models/query_params.dart';
 import 'package:catbiblio_app/models/book_preview.dart';
 
 class SruService {
+  static const String _baseUrl = 'http://148.226.6.25';
   // ejemplo: http://{{baseUrl}}/cgi-bin/koha/svc/bibliosItems?title=dunde&branch=USBI-X
   // ejemplo: http://{{baseUrl}}/cgi-bin/koha/svc/bibliosItems?author=frank+herbert
   // ejemplo: http://{{baseUrl}}/cgi-bin/koha/svc/bibliosItems?subject=ciencia+ficcion&branch=USBI-V
+
   static Future<List<BookPreview>> searchBooks(QueryParams params) async {
-    final String _baseUrl = 'http://148.226.6.25';
     final queryParameters = {
       'title': params.searchBy == 'title' ? params.searchQuery : null,
       'author': params.searchBy == 'author' ? params.searchQuery : null,
@@ -20,8 +21,6 @@ class SruService {
       'issn': params.searchBy == 'issn' ? params.searchQuery : null,
       'branch': params.library.isNotEmpty ? params.library : null,
     }..removeWhere((key, value) => value == null || value.isEmpty);
-
-    print('Query Parameters: $queryParameters');
 
     final url = Uri.parse(
       '$_baseUrl/cgi-bin/koha/svc/bibliosItems',
