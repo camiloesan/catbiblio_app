f5cd1bbdd1c754ba3d91b3aac65a78d3bbdbb80e
Luis Casas <zs21013875@estudiantes.uv.mx>
2025-09-25 14:34:38 -0600

    Add SearchResult model to encapsulate search results with books and total records

---
+++ lib/models/search_result.dart
@@ -0,0 +1,8 @@
+import 'package:catbiblio_app/models/book_preview.dart';
+
+class SearchResult {
+  final List<BookPreview> books;
+  final int totalRecords;
+
+  SearchResult({required this.books, required this.totalRecords});
+}
