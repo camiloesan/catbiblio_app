d62ead8157c29e900607364a4e397aaad385d4cb
Luis Casas <zs21013875@estudiantes.uv.mx>
2025-10-20 15:10:51 -0600

    Refactor BookPreview constructor and fromJson method for improved clarity and default values

--- lib/models/book_preview.dart
+++ lib/models/book_preview.dart
@@ -4,16 +4,39 @@ class BookPreview {
   String coverUrl;
   String biblioNumber;
   String publishingDetails;
+  int totalRecords = 0;
   int locatedInLibraries = 0;
 
   BookPreview({
     required this.title,
-    required this.author,
+    this.author = '',
     required this.coverUrl,
     required this.biblioNumber,
-    required this.publishingDetails,
+    this.publishingDetails = '',
     required this.locatedInLibraries,
+    required this.totalRecords,
   });
+
+  factory BookPreview.fromJson(Map<String, dynamic> json) {
+    return BookPreview(
+      title: json['full_title'] as String,
+      author: json['author'] as String? ?? '',
+      coverUrl: '',
+      biblioNumber: json['biblionumber'] as String,
+      publishingDetails:
+          ((json['place']?.toString().trim() ?? '').isNotEmpty &&
+              (json['publishercode']?.toString().trim() ?? '').isNotEmpty &&
+              (json['copyrightdate']?.toString().trim() ?? '').isNotEmpty)
+          ? [
+              json['place']!.toString().trim(),
+              json['publishercode']!.toString().trim(),
+              json['copyrightdate']!.toString().trim(),
+            ].join(' ')
+          : '',
+      locatedInLibraries: json['located_in_libraries'] as int? ?? 0,
+      totalRecords: json['total_results'] as int? ?? 0,
+    );
+  }
 
   @override
   String toString() {
