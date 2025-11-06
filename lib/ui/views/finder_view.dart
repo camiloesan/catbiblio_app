deb79fcf911d09440e09169d7122634eab72e882
Camilo <camiloesan@gmail.com>
2025-11-06 11:53:12 -0600

    refactor finder view to adjust for screens dynamically

--- lib/ui/views/finder_view.dart
+++ lib/ui/views/finder_view.dart
@@ -22,190 +22,272 @@ class _FinderViewState extends FinderController {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(AppLocalizations.of(context)!.finderTitle)),
-      body: SingleChildScrollView(
-        child: Center(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: MediaQuery.of(context).size.height,
-              maxWidth: MediaQuery.of(context).size.width < screenSizeLimit
-                  ? MediaQuery.of(context).size.width
-                  : (MediaQuery.of(context).size.width / 3) * 2,
-            ),
-            child: Column(
-              children: [
-                Container(
-                  color: primaryUVColor,
-                  padding: const EdgeInsets.all(16.0),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < screenSizeLimit) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  TitleSection(
+                    widget: widget,
+                    bookLocation: bookLocation,
+                  ),
+                  MapSection(
+                    widget: widget,
+                    bookLocation: bookLocation,
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return SingleChildScrollView(
+              child: Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width < screenSizeLimit
+                        ? MediaQuery.of(context).size.width
+                        : (MediaQuery.of(context).size.width / 4) * 3.5,
+                  ),
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      FutureBuilder<Image?>(
-                        future: ImageService.fetchThumbnailImageUrl(
-                          widget.params.biblioNumber,
+                      Expanded(
+                        child: TitleSection(
+                          widget: widget,
+                          bookLocation: bookLocation,
                         ),
-                        builder: (context, snapshot) {
-                          if (snapshot.hasError || snapshot.data == null) {
-                            return const SizedBox.shrink();
-                          } else {
-                            return Row(
-                              children: [
-                                SizedBox(width: 120, child: snapshot.data!),
-                                const SizedBox(width: 16.0),
-                              ],
-                            );
-                          }
-                        },
                       ),
                       Expanded(
-                        child: Text(
-                          '${widget.params.title}\n\n${AppLocalizations.of(context)!.classification}:\n${widget.params.classification}',
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
-                          ),
+                        child: MapSection(
+                          widget: widget,
+                          bookLocation: bookLocation,
                         ),
                       ),
                     ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 0.0,
-                    left: 16.0,
-                    right: 16.0,
-                    bottom: 16.0,
-                  ),
-                  child: Column(
-                    spacing: 4.0,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 8.0),
-                      Text(
-                        AppLocalizations.of(context)!.location,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        widget.params.holdingLibrary,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-                      IntrinsicHeight(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  Icon(
-                                    Icons.location_on,
-                                    color: Theme.of(
-                                      context,
-                                    ).colorScheme.primary,
-                                    size: 24,
-                                  ),
-                                  Text(
-                                    AppLocalizations.of(context)!.level,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    bookLocation.level,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const VerticalDivider(),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
 
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  Icon(
-                                    Icons.meeting_room,
-                                    color: Theme.of(
-                                      context,
-                                    ).colorScheme.primary,
-                                    size: 24,
-                                  ),
-                                  Text(
-                                    AppLocalizations.of(context)!.room,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    bookLocation.room,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const VerticalDivider(),
+class MapSection extends StatelessWidget {
+  const MapSection({
+    super.key,
+    required this.widget,
+    required this.bookLocation,
+  });
 
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  Icon(
-                                    Icons.library_books,
-                                    color: Theme.of(
-                                      context,
-                                    ).colorScheme.primary,
-                                    size: 24,
-                                  ),
-                                  Text(
-                                    AppLocalizations.of(context)!.collection,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    widget.params.collection,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      InteractiveViewer(
-                        scaleEnabled: kIsWeb ? false : true,
-                        panEnabled: true,
-                        minScale: 0.5,
-                        maxScale: 4,
-                        child: Image.asset('assets/images/croquis1.png'),
-                      ),
-                    ],
+  final FinderView widget;
+  final BookLocation bookLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 0.0, 
+        left: 16.0,
+        right: 16.0,
+        bottom: 16.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InteractiveViewer(
+            scaleEnabled: kIsWeb ? false : true,
+            panEnabled: true,
+            minScale: 0.5,
+            maxScale: 4,
+            child: Image.asset('assets/images/croquis1.png'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.widget,
+    required this.bookLocation,
+  });
+
+  final FinderView widget;
+  final BookLocation bookLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          color: primaryUVColor,
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FutureBuilder<Image?>(
+                future: ImageService.fetchThumbnailImageUrl(
+                  widget.params.biblioNumber,
+                ),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError || snapshot.data == null) {
+                    return const SizedBox.shrink();
+                  } else {
+                    return Row(
+                      children: [
+                        SizedBox(width: 120, child: snapshot.data!),
+                        const SizedBox(width: 16.0),
+                      ],
+                    );
+                  }
+                },
+              ),
+              Expanded(
+                child: Text(
+                  '${widget.params.title}\n\n${AppLocalizations.of(context)!.classification}:\n${widget.params.classification}',
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      ),
+
+        Padding(
+          padding: EdgeInsetsGeometry.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 8.0),
+              Align(
+                alignment: AlignmentGeometry.centerLeft,
+                child: Text(
+                  AppLocalizations.of(context)!.location,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8.0,),
+              Wrap(
+                children: [
+                  Icon(
+                    Icons.location_city,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  const SizedBox(width: 8.0,),
+                  Text(
+                    widget.params.holdingLibrary,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            color: Theme.of(context).colorScheme.primary,
+                            size: 24,
+                          ),
+                          Text(
+                            AppLocalizations.of(context)!.level,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            bookLocation.level,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const VerticalDivider(),
+
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.meeting_room,
+                            color: Theme.of(context).colorScheme.primary,
+                            size: 24,
+                          ),
+                          Text(
+                            AppLocalizations.of(context)!.room,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            bookLocation.room,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const VerticalDivider(),
+
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.library_books,
+                            color: Theme.of(context).colorScheme.primary,
+                            size: 24,
+                          ),
+                          Text(
+                            AppLocalizations.of(context)!.collection,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            widget.params.collection,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
