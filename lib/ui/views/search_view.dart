137e09e4fed0fe9cd9729c8af33654264e205698
Luis Casas <zs21013875@estudiantes.uv.mx>
2025-11-07 12:38:13 -0600

    remove unused import from search_view.dart

--- lib/ui/views/search_view.dart
+++ lib/ui/views/search_view.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:catbiblio_app/l10n/app_localizations.dart';
 import 'package:catbiblio_app/models/book_preview.dart';
